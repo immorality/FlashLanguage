63a1af388e3de98477cb2034b52d4a46dae1b118
immorality <immorality7@gmail.com>
2016-04-23 13:02:43 +0200

    changing connection structure and add some gui functional

--- src/ConnectDB.py
+++ src/ConnectDB.py
@@ -1,28 +1,95 @@
-
 from mysql.connector import MySQLConnection
 from mysql.connector import Error
 
 
-def connect():
-    try:
-        conn = MySQLConnection(host='localhost', database='mydb', user='root', password='root')
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users")
-        row = cursor.fetchone()
+class ConnectDB:
+    def __init__(self):
+        pass
 
-        while row is not None:
-            print row
-            row = cursor.fetchone()
+    def iter_row(self, cursor, size=10):
+        while True:
+            rows = cursor.fetchmany(size)
+            if not rows:
+                break
+            for row in rows:
+                yield row
 
-        if conn.is_connected():
-            print "Connected to database"
+    def getUsers(self):
+        testList = []
 
-    except Error as e:
-        print e
+        try:
+            conn = MySQLConnection(host='localhost', database='mydb', user='root', password='root')
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM users")
+            # rows = cursor.fetchall()
 
-    finally:
-        cursor.close()
-        conn.close()
+            # print rows
+
+            for row in self.iter_row(cursor, 10):
+                testList += row
+                print row
+
+            if conn.is_connected():
+                print "Connected to database"
+
+        except Error as e:
+            print e
+
+        finally:
+            cursor.close()
+            conn.close()
+
+        return testList
+
+    def insert_new(self, user, password):
+        query = "INSERT INTO users(name, password) " \
+                "VALUES(%s,%s)"
+        args = (user, password)
+
+        try:
+            conn = MySQLConnection(host='localhost', database='mydb', user='root', password='root')
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+
+            if cursor.lastrowid:
+                print "Last row id: ", cursor.lastrowid
+            else:
+                print "Last row is not found"
+
+            conn.commit()
+
+        except Error as error:
+            print error
+
+        finally:
+            cursor.close()
+            conn.close()
+
+    def insertEnglishWord(self, word):
+        query = "INSERT INTO english(word) " \
+                "VALUES(%s)"
+        args = (word)
+
+        try:
+            conn = MySQLConnection(host='localhost', database='mydb', user='root', password='root')
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+
+            conn.commit()
+
+        except Error as error:
+            print error
+
+        finally:
+            cursor.close()
+            conn.close()
+
+
+def main():
+    connection = ConnectDB()
+    print connection.getUsers()
+    # insert_new("John", "1q2w3e4r")
+
 
 if __name__ == '__main__':
-    connect()
+    main()
--- src/generalStage.py
+++ src/generalStage.py
@@ -1,11 +1,20 @@
 from Tkinter import *
 from ttk import *
+from ConnectDB import *
+
+
+def showUsers():
+     for x in getUsers():
+        tx.insert(2.0, x + " ")
+
 
 generalStage = Tk()
 generalStage.iconbitmap('icons_7457.ico')
 generalStage.title('FlashLanguage')
 generalStage.geometry('800x600+300+50')
 
-Label(generalStage, text="Hello, retard!").pack()
-butt = Button(text="Hei!", width=30)
+tx = Text(font=('times', 12), width=50, height=15, wrap=WORD)
+tx.pack(expand=YES)
+
+butt = Button(text="Hei!", width=30, command=showUsers)
 butt.pack()
